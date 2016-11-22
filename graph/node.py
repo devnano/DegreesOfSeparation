d1737bed39a77c0141f901f2345fe006a71cba8f
Mariano Heredia <mariano@kartjuba.com>
2016-11-22 18:02:56 -0300

    Implemented generate random links.

--- graph/node.py
+++ graph/node.py
@@ -9,11 +9,28 @@ class Node:
 
 # generation code
 
+from random import randrange
+
 generated_link_base_name = "generated_link_base_name_%d"
 
 def generate_link_name(index):
     return generated_link_base_name % index
 
 def generate_unique_links(n):
-    links = {generate_link_name(x + 1) for x in range(1, n)}
+    links = {generate_link_name(x + 1) for x in range(0, n)}
     return links
+
+def generate_random_links(source_links, n):
+    child_links = set()
+    while not len(child_links) == n:
+        link = source_links[randrange(0, len(source_links))]
+        child_links.add(link)
+    
+    return child_links
+
+def generate_node_random_links(node, source_links, n):
+    child_links = generate_random_links(source_links, n)
+
+    for child_link in child_links:
+        child_node = Node(child_link)
+        node.add_child(child_node)
--- graph/test/test_node.py
+++ graph/test/test_node.py
@@ -18,6 +18,9 @@ def test_is_child_after_add():
 
 # generation code test
 
+source_links = ['generated_link_base_name_20', 'generated_link_base_name_17', 'generated_link_base_name_23', 'generated_link_base_name_38', 'generated_link_base_name_16', 'generated_link_base_name_27', 'generated_link_base_name_36', 'generated_link_base_name_49', 'generated_link_base_name_11', 'generated_link_base_name_21', 'generated_link_base_name_30', 'generated_link_base_name_10', 'generated_link_base_name_26', 'generated_link_base_name_6', 'generated_link_base_name_5', 'generated_link_base_name_4', 'generated_link_base_name_35', 'generated_link_base_name_40', 'generated_link_base_name_37', 'generated_link_base_name_25', 'generated_link_base_name_13', 'generated_link_base_name_22', 'generated_link_base_name_50', 'generated_link_base_name_43', 'generated_link_base_name_9', 'generated_link_base_name_8', 'generated_link_base_name_34', 'generated_link_base_name_41', 'generated_link_base_name_46', 'generated_link_base_name_29', 'generated_link_base_name_32', 'generated_link_base_name_18', 'generated_link_base_name_42', 'generated_link_base_name_28', 'generated_link_base_name_48', 'generated_link_base_name_44', 'generated_link_base_name_12', 'generated_link_base_name_19', 'generated_link_base_name_3', 'generated_link_base_name_45', 'generated_link_base_name_39', 'generated_link_base_name_7', 'generated_link_base_name_15', 'generated_link_base_name_47', 'generated_link_base_name_2', 'generated_link_base_name_31', 'generated_link_base_name_33', 'generated_link_base_name_24', 'generated_link_base_name_14']
+
+
 def test_generate_link_name():
     link_name = generate_link_name(1)
     assert "generated_link_base_name_1" == link_name
@@ -25,9 +28,20 @@ def test_generate_link_name():
 def test_generate_unique_links_len():
     n = 100
     unique_links = generate_unique_links(n)
-    assert len(unique_links)
+    assert len(unique_links) == n
 
 def test_generate_unique_links_type():
     n = 1
     unique_links = generate_unique_links(n)
     assert isinstance(unique_links, set)
+
+def test_generate_random_links():
+    n = 10
+    random_links = generate_random_links(source_links, n)
+    assert len(random_links) == n
+
+def test_generate_node_random_links():
+    n = 10
+    root_node = Node("")
+    generate_node_random_links(root_node, source_links, n)
+    assert len(root_node._children_set) == n
