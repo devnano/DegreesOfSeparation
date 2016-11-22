f988344344adf386998c378687f2c900274a7cfb
Mariano Heredia <mariano@kartjuba.com>
2016-11-22 14:56:56 -0300

    Implementing tests.

--- graph/node.py
+++ graph/node.py
@@ -1,10 +1,9 @@
 class Node:
     """Represents a Node in the Graph."""
     def __init__(self, url):
-        self._pointed_nodes_set = set()
-        self._pointed_by_nodes_set = set()
+        self._children_set = set()
         self._url = url
 
-    def add_pointed_node(self, node):
-        self._pointed_nodes_set.add(node)
-        node._pointed_by_nodes_set.add(self)
+    def add_child(self, node):
+        self._children_set.add(node)
+
--- graph/test/test_node.py
+++ graph/test/test_node.py
@@ -5,22 +5,12 @@ def test_url_correct_after_creation():
     n = Node(link)
     assert n._url == link
 
-def test_pointed_nodes_after_creation():
+def test_children_set_after_creation():
     n = Node("")
-    assert n._pointed_nodes_set is not None
-
-def test_pointed_by_nodes_after_creation():
-    n = Node("")
-    assert n._pointed_by_nodes_set is not None
+    assert n._children_set is not None
 
 def test_pointed_by_nodes_after_add():
     n1 = Node("")
     n2 = Node("")
-    n1.add_pointed_node(n2)
-    assert n1 in n2._pointed_by_nodes_set
-
-def test_pointed_nodes_after_add():
-    n1 = Node("")
-    n2 = Node("")
-    n1.add_pointed_node(n2)
-    assert n2 in n1._pointed_nodes_set
+    n1.add_child(n2)
+    assert n2 in n1._children_set
